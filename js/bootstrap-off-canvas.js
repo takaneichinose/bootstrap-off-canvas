fd117839bcd53604ff0e0a09084c1cda77529d55
Takane Ichinose <ichinose.takane@gmail.com>
2022-10-04 10:06:43 +0900

    Added close on content click for floating navigation.

--- js/bootstrap-off-canvas.js
+++ js/bootstrap-off-canvas.js
@@ -3,19 +3,17 @@
 $(function() {
   $('.off-canvas').on('show.bs.off-canvas', function() {
     $(this).addClass('active')
-    $(this).find('.off-canvas-content').addClass('active')
   })
 
   $('.off-canvas').on('hide.bs.off-canvas', function() {
     $(this).removeClass('active')
-    $(this).find('.off-canvas-content').removeClass('active')
   })
 
   $('.off-canvas-toggler').on('click', function(evt) {
     evt.stopPropagation()
     evt.preventDefault()
 
-    var elm = $(this).data('target')
+    let elm = $(this).data('target')
 
     if (!$(elm).hasClass('active')) {
       $(elm).trigger('show.bs.off-canvas')
@@ -26,7 +24,7 @@ $(function() {
   })
 
   $('.off-canvas-content:not(.off-canvas-no-close)').on('click', function(evt) {
-    var elm = $(this).data('target')
+    let elm = $(this).data('target')
 
     if ($(elm).hasClass('active')) {
       evt.stopPropagation()
